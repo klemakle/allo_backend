7ea0676af48122bfe9aaf9d3d9ac04ab21caf0a2
klemakle <kalidou1309@gmail.com>
2021-08-16 17:10:02 +0000

    first config done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './api/auth/auth.module';
+import { DriverModule } from './api/driver/driver.module';
+import { PassengerModule } from './api/passenger/passenger.module';
+import { TravelModule } from './api/travel/travel.module';
+import config from './config/env.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    AuthModule,
+    DriverModule,
+    PassengerModule,
+    TravelModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
