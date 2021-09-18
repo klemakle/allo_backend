94aadfa4a4f652fa8b797a11e72567d1abac065b
klemakle <kalidou1309@gmail.com>
2021-09-18 17:17:54 +0000

    travel model => some labels added

--- src/api/travel/model/travel.model.ts
+++ src/api/travel/model/travel.model.ts
@@ -12,8 +12,39 @@ export const TravelSchema = new mongoose.Schema({
     trim: true,
     required: true,
   },
-  tarif: {
+  price: {
     type: String,
     default: '0',
+  },
+  departureTime: {
+    type: Date,
+    default: Date,
+    index: true,
+  },
+  driver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'driver',
+  },
+  passenger: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'passenger',
+    },
+  ],
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+  created_at: {
+    type: Date,
+    default: Date,
+  },
+  updated: {
+    type: Date,
+    default: Date,
   },
 });
